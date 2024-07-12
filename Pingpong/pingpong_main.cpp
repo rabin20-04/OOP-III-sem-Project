859115b6a488abe1b75a4c52539a4344dd584590
Sujan_Adhikari <079bct086@ioepc.edu.np>
2024-07-12 17:50:31 +0545

    dvelopment of the AI for the game

--- Pingpong/pingpong_main.cpp
+++ Pingpong/pingpong_main.cpp
@@ -26,14 +26,85 @@ public:
 
 
         if (x+radius>=GetScreenWidth() || x-radius<=0) { // for left and right side obstruction
-            speed_x*=-1
+            speed_x*=-1;
         }
             
         }
     }
 };
 
+class Paddle{
+    protected:
+    void avoidObstruction(){
+
+     if (y <= 0)
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+}
+
+public:
+    float x, y;
+    int speed;
+    int width, height;
+
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void move()
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            y -= speed;
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y += speed;
+        }
+
+       avoidObstruction();
+    }
+
+
+};
+class cpuPaddle: public Paddle
+{
+
+    public:
+    void move(int bally)
+    {
+        if (y + height/2 < bally)
+        {
+            y += speed;
+        }
+        if (y + height/2 > bally)
+        {
+            y -= speed;
+        }
+
+        if (y <= 0)
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+        avoidObstruction();
+    }
+
+};
+
 Ball ball;
+Paddle player;
+
+cpuPaddle cpu;
 
 int main()
 {
@@ -50,6 +121,19 @@ int main()
     ball.y = screenHeight / 2;
     ball.speed_x = 7;
     ball.speed_y = 7;
+    
+    player.width = 25;
+    player.height = 120;
+    player.x = screenWidth-player.width-12;
+    player.y = screenHeight/2 - player.height/2;
+    player.speed = 6;
+
+    cpu.width = 25;
+    cpu.height = 120;  
+    cpu.x = 12;
+    cpu.y = screenHeight/2 - cpu.height/2;
+    cpu.speed = 6;
+
 
     while (WindowShouldClose() == false)
     {                   // the game will run until window function get false value either from windows close or esc key
@@ -59,14 +143,16 @@ ClearBackground(BLACK); // this function will clear the background of the canvas
 
         ball.move();
         // remember that the coordinate system in raylib starts from the top left corner of the screen updown :y side: x
-
+player.move();
+cpu.move(ball.y);
         DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE); // x1 y1 x2 y2 color
 
         ball.Draw();
+        player.Draw();
+        cpu.Draw();
        
         DrawRectangle(12, screenHeight / 2 - 60, 25, 120, WHITE); // x y width height color
 
-        DrawRectangle(screenWidth - 37, screenHeight / 2 - 60, 25, 120, WHITE);
  
 
         EndDrawing(); // this function will end the drawing and display the canva
