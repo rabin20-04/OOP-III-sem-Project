cb79b2959cff507589747cfe5c20436ef499d06e
Sujan_Adhikari <079bct086@ioepc.edu.np>
2024-07-12 15:44:16 +0545

    ball move movement set to 5pxl/sec

--- Pingpong/pingpong_main.cpp
+++ Pingpong/pingpong_main.cpp
@@ -1,82 +1,66 @@
-//yo arko version ko ho where there been no use of the GUI
+#include <iostream>
+#include <raylib.h> /// Raylib is a library for making games
 
-
-// I have left it so may be I could work on it later for better unnderstanding of the project
-
-#include<iostream>
 using namespace std;
-enum eDir { STOP = 0, LEFT =1, UPLEFT =2, DOWNLEFT=3, RIGHT=4, UPRIGHT=5, DOWNRIGHT=6};
-//enum is the just a way to store the value
 
-class cball
+class Ball
 {
-
-private:
-int x,y;//to store the position of the ball
-int originalX, originalY;
-eDir direction;
-
-
 public:
-cball(int posX, int posY)
-{
-originalX = posX;
-originalY = posY;
-x=posX; 
-y=posY;
-direction = STOP;
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
 
-}
+    void Draw()
+    {
+        DrawCircle(x, y, radius, WHITE);
+    }
 
-void Reset()
-{
-
-x = originalX; y = originalY;
-direction = STOP;
-}
-
-void changeDirection(edir d)
-{
-
-    direction = d;
-}
-void randomDirection()
-{
-  direction = (eDir) ((rand() %6 )+)1; //random number from 1 to 6
-
-}
-
-    inline int getX() { return x;}
-    inline int getY() { return y;}
-    inline eDir getDirection () { return direction;}
-
-     void Move()
-     {
-        switch (direction)
-        {
-            case STOP:
-            break;
-
-            case LEFT:
-            x--;
-            break;
-
-            case RIGHT:
-            x++;
-            break;
-
-
-        }
-
-
-     }
+    // function to move the ball
+    void move()
+    {
+        x += speed_x;
+        y += speed_y;
+    }
 };
 
+Ball ball;
 
+int main()
+{
+    cout << "Starting the game" << endl;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+    InitWindow(screenWidth, screenHeight, "Ping Pong");
 
+    SetTargetFPS(60); // this function will set the frame rate of the game to 60 frames per second
+    // if we didn't set the frame rate the game will run as fast as the computer can handle
 
-int main (){
+    ball.radius = 20;
+    ball.x = screenWidth / 2;
+    ball.y = screenHeight / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
 
+    while (WindowShouldClose() == false)
+    {                   // the game will run until window function get false value either from windows close or esc key
+        BeginDrawing(); // this function creates a blankcanvas so that we can starting drawinng
 
+        ClearBackground(BLACK); // this function will clear the background of the canvas and set it to black
+
+        ball.move();
+        // remember that the coordinate system in raylib starts from the top left corner of the screen updown :y side: x
+
+        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE); // x1 y1 x2 y2 color
+
+        ball.Draw();
+
+        DrawRectangle(12, screenHeight / 2 - 60, 25, 120, WHITE); // x y width height color
+
+        DrawRectangle(screenWidth - 37, screenHeight / 2 - 60, 25, 120, WHITE);
+
+        EndDrawing(); // this function will end the drawing and display the canva
+    }
+
+    CloseWindow();
     return 0;
 }
