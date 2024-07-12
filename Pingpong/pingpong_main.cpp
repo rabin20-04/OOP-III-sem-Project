91ce25482a6fcd7a8c543ab17f1d125063388681
Sujan_Adhikari <079bct086@ioepc.edu.np>
2024-07-12 16:29:34 +0545

    ball bounce back from the wall

--- Pingpong/pingpong_main.cpp
+++ Pingpong/pingpong_main.cpp
@@ -20,6 +20,16 @@ public:
     {
         x += speed_x;
         y += speed_y;
+
+        if (y+radius>=GetScreenHeight() || y-radius<=0) { // checking if the ball touch the bottom or top of the screen
+        speed_y*=-1; //changing the direction of the ball in y direction
+
+
+        if (x+radius>=GetScreenWidth() || x-radius<=0) { // for left and right side obstruction
+            speed_x*=-1
+        }
+            
+        }
     }
 };
 
@@ -45,7 +55,7 @@ int main()
     {                   // the game will run until window function get false value either from windows close or esc key
         BeginDrawing(); // this function creates a blankcanvas so that we can starting drawinng
 
-        ClearBackground(BLACK); // this function will clear the background of the canvas and set it to black
+ClearBackground(BLACK); // this function will clear the background of the canvas and set it to black
 
         ball.move();
         // remember that the coordinate system in raylib starts from the top left corner of the screen updown :y side: x
@@ -53,10 +63,11 @@ int main()
         DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE); // x1 y1 x2 y2 color
 
         ball.Draw();
-
+       
         DrawRectangle(12, screenHeight / 2 - 60, 25, 120, WHITE); // x y width height color
 
         DrawRectangle(screenWidth - 37, screenHeight / 2 - 60, 25, 120, WHITE);
+ 
 
         EndDrawing(); // this function will end the drawing and display the canva
     }
