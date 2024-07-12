2c3909a80d64ffa42a14bcbdf84c857ce8057461
Sujan_Adhikari <079bct086@ioepc.edu.np>
2024-07-12 08:25:24 +0545

    ball creation

--- Pingpong/pingpong_main.cpp
+++ Pingpong/pingpong_main.cpp
@@ -1,6 +1,75 @@
 #include<iostream>
 
 using namespace std;
+enum eDir { STOP = 0, LEFT =1, UPLEFT =2, DOWNLEFT=3, RIGHT=4, UPRIGHT=5, DOWNRIGHT=6};
+//enum is the just a way to store the value
+
+class cball
+{
+
+private:
+int x,y;//to store the position of the ball
+int originalX, originalY;
+eDir direction;
+
+
+public:
+cball(int posX, int posY)
+{
+originalX = posX;
+originalY = posY;
+x=posX; 
+y=posY;
+direction = STOP;
+
+}
+
+void Reset()
+{
+
+x = originalX; y = originalY;
+direction = STOP;
+}
+
+void changeDirection(edir d)
+{
+
+    direction = d;
+}
+void randomDirection()
+{
+  direction = (eDir) ((rand() %6 )+)1; //random number from 1 to 6
+
+}
+
+    inline int getX() { return x;}
+    inline int getY() { return y;}
+    inline eDir getDirection () { return direction;}
+
+     void Move()
+     {
+        switch (direction)
+        {
+            case STOP:
+            break;
+
+            case LEFT:
+            x--;
+            break;
+
+            case RIGHT:
+            x++;
+            break;
+
+
+        }
+
+
+     }
+};
+
+
+
 
 int main (){
 
