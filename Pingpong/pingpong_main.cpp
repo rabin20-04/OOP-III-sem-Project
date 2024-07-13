e2546c73690b00443786e9850059c1e2045f53f6
Sujan_Adhikari <079bct086@ioepc.edu.np>
2024-07-13 14:02:41 +0545

    sound fix

--- Pingpong/pingpong_main.cpp
+++ Pingpong/pingpong_main.cpp
@@ -17,7 +17,7 @@ public:
 
     void Draw()
     {
-        DrawCircle(x, y, radius,WHITE);
+        DrawCircle(x, y, radius, WHITE);
     }
 
     // function to move the ball
@@ -25,36 +25,37 @@ public:
     {
         x += speed_x;
         y += speed_y;
-        Sound lose =  LoadSound("resources/lose.wav");
-        Sound win =  LoadSound("resources/win.wav");
+        Sound lose = LoadSound("resources/lose.wav");
+        Sound win = LoadSound("resources/win.wav");
 
         if (y + radius >= GetScreenHeight() || y - radius <= 0)
         {                  // checking if the ball touch the bottom or top of the screen
             speed_y *= -1; // changing the direction of the ball in y direction
 
-            if (x + radius >= GetScreenWidth()){
+            if (x + radius >= GetScreenWidth())
+            {
                 PlaySound(lose);
                 cpu_score++; // computer wins
                 resetball();
                 // UnloadSound(lose);
             }
-                
 
-            if (x - radius <= 0){
+            if (x - radius <= 0)
+            {
                 PlaySound(win);
-                player_score++; 
-            resetball(); 
-            // UnloadSound(win); 
-            }          
+                player_score++;
+                resetball();
+                // UnloadSound(win);
+            }
         }
     }
 
-    void resetball(){
+    void resetball()
+    {
         x = GetScreenWidth() / 2;
         y = GetScreenHeight() / 2;
         speed_x *= -1;
         speed_y *= -1;
-    
     }
 };
 
@@ -81,7 +82,7 @@ public:
 
     void Draw()
     {
-    DrawRectangleRounded(Rectangle{x, y, width,height}, 0.8,0, DarkBlue);
+        DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, DarkBlue);
     }
 
     void move()
@@ -160,56 +161,49 @@ int main()
     cpu.y = screenHeight / 2 - cpu.height / 2;
     cpu.speed = 1;
 
-//loading of the sound
-Sound strike = LoadSound("resources/strike.wav");
+    // loading of the sound
+    Sound strike = LoadSound("resources/strike.wav");
 
-    while (WindowShouldClose() == false)//// the game will run until window function get false value either from windows close or esc key
-    {                   
-
-
-
+    while (WindowShouldClose() == false) //// the game will run until window function get false value either from windows close or esc key
+    {
 
         BeginDrawing(); // this function creates a blankcanvas so that we can starting drawinng
 
         ClearBackground(SkyBlue); // this function will clear the background of the canvas and set it to black
 
-        
-        DrawRectangle(screenWidth / 2, 0, 2, screenHeight, WHITE); // x y width height color
-        DrawCircle(screenWidth / 2, screenHeight / 2, 90, DarkBlue);    // x y radius color
+        DrawRectangle(screenWidth / 2, 0, 2, screenHeight, WHITE);   // x y width height color
+        DrawCircle(screenWidth / 2, screenHeight / 2, 90, DarkBlue); // x y radius color
         ball.move();
         // remember that the coordinate system in raylib starts from the top left corner of the screen updown :y side: x
         player.move();
         cpu.move(ball.y);
 
         // check for the colloision between the ball and the player
-       if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
-          { ball.speed_x *= -1;
-          PlaySound(strike);
-          }
-          if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
-          { ball.speed_x *= -1;
-           PlaySound(strike);
-          }
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+        {
+            ball.speed_x *= -1;
+            PlaySound(strike);
+        }
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+        {
+            ball.speed_x *= -1;
+            PlaySound(strike);
+        }
 
-
-
-          // slide  and middle lines for the game interface
-        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE); // x1 y1 x2 y2 color
-         DrawLine(2, 2, screenWidth , 2, WHITE); // x1 y1 x2 y2 color
-                  DrawLine(0, screenHeight-2, screenWidth, screenHeight-2, WHITE); // x1 y1 x2 y2 color
-
-
+        // slide  and middle lines for the game interface
+        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);  // x1 y1 x2 y2 color
+        DrawLine(2, 2, screenWidth, 2, WHITE);                               // x1 y1 x2 y2 color
+        DrawLine(0, screenHeight - 2, screenWidth, screenHeight - 2, WHITE); // x1 y1 x2 y2 color
 
         ball.Draw();
         player.Draw();
         cpu.Draw();
-        DrawText(TextFormat("CPU:%i", cpu_score), screenWidth / 4 - 20, 20, 50, WHITE); // text x y font size color
-        DrawText(TextFormat("YOU:%i", player_score), 3*screenWidth/4 - 20, 20, 50, WHITE);  // text x y font size colo
+        DrawText(TextFormat("CPU:%i", cpu_score), screenWidth / 4 - 20, 20, 50, WHITE);        // text x y font size color
+        DrawText(TextFormat("YOU:%i", player_score), 3 * screenWidth / 4 - 20, 20, 50, WHITE); // text x y font size colo
 
         EndDrawing(); // this function will end the drawing and display the canva
     }
-CloseAudioDevice();
+    CloseAudioDevice();
     CloseWindow();
     return 0;
-
 }
