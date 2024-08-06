819f556047ff914817e694a8805e3d01dac9b176
Samir Acharya <079bct070@ioepc.edu.np>
2024-08-07 00:41:30 +0545

    multiplay code almost done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h> /// Raylib is a library for making game
+#include <vector>
 
 using namespace std;
 
@@ -7,8 +8,9 @@ Color Green = Color{38, 184, 155 , 255};
 Color Blue = Color{45, 110, 230, 255};
 Color DarkBlue  = Color{25, 50, 180, 255};
 Color LightB = Color{72, 146, 220 ,255};
-Color Yellow= Color{243 , 213 , 91, 255};
-Color roun= Color{141 , 188 , 232 , 255};
+Color Yellow = Color{243 , 213 , 91, 255};
+Color roun = Color{141 , 188 , 232 , 255};
+Color skyblue = Color{179 , 235 , 235 , 255};
 
 
 
@@ -109,7 +111,7 @@ void update(){
 
     int rounds(){
 
-        return round;
+        return round ;
     }
 
 
@@ -206,12 +208,7 @@ class player2 :public paddle{
     player2(){}
     //using concept of constructor in single inheratance
     player2(float a, float b,float w,float h) :paddle (a,b,w,h){
-        /* x=a;
-         y=b;
-         width=w;
-         height=h;
-         speed=6;
-*/
+    
     }
 
      void update(){
@@ -230,22 +227,92 @@ class player2 :public paddle{
       limit();
    }
 
-    /*void update(float by){
-        if ( y+ height >= by){
-
-            y = y - speed;
-         }
-         else if (y + height <= by){
-
-            y = y + speed;
-         }
-
-        limit();
-    }
-  */
-
-
+    
 };
+
+ // Define the AnimateBall class here
+class AnimateBall
+{
+private:
+    Vector2 position;
+    float radius;
+    Color ballColor; 
+    float speed;
+
+public:
+    // Parameterized Constructor
+    AnimateBall(Vector2 pos, float r, Color c, float s)
+        : position(pos), radius(r), ballColor(c), speed(s) {}
+
+    // Default Constructor
+    AnimateBall()
+        : position({0.0f, 0.0f}), radius(5.0f), ballColor(RAYWHITE), speed(0.0f) {}
+
+    // Setter methods
+    void SetPosition(Vector2 pos) { position = pos; }
+    void SetRadius(float r) { radius = r; }
+    void SetColor(Color c) { ballColor = c; } // Updated setter
+    void SetSpeed(float s) { speed = s; }
+
+    // Getter methods
+    Vector2 GetPosition() const { return position; }
+    float GetRadius() const { return radius; }
+    Color GetColor() const { return ballColor; } // Updated getter
+    float GetSpeed() const { return speed; }
+
+    void animation()
+    {
+        int screenWidth = 1280;
+        int screenHeight = 720;
+        std::vector<AnimateBall> balls;
+
+        // Initialize balls with parameterized constructor
+        for (int i = 0; i < 40; i++)
+        {
+            Vector2 pos = {(float)GetRandomValue(0, screenWidth), 0};
+            float r = (float)GetRandomValue(5, 10); // Reduced size
+            Color c = {(unsigned char)GetRandomValue(100, 255), (unsigned char)GetRandomValue(100, 255), (unsigned char)GetRandomValue(100, 255), 255};
+            float s = (float)GetRandomValue(50, 100) / 10.0f;
+
+            AnimateBall ball(pos, r, c, s);
+            balls.push_back(ball);
+        }
+
+        while (!WindowShouldClose())
+        {
+            // Update
+            for (auto &ball : balls)
+            {
+                Vector2 pos = ball.GetPosition();
+                pos.y += ball.GetSpeed(); // Move balls downwards
+                if (pos.y > screenHeight + ball.GetRadius())
+                {
+                    pos.y = -ball.GetRadius();                          // Reset position to top if it goes beyond the bottom
+                    pos.x = (float)GetRandomValue(0, screenWidth);     // Randomize horizontal position
+                    ball.SetPosition(pos);
+                }
+                else
+                {
+                    ball.SetPosition(pos);
+                }
+            }
+
+            // Draw
+            BeginDrawing();
+            ClearBackground(skyblue);
+
+            for (const auto &ball : balls)
+            {
+                DrawCircleV(ball.GetPosition(), ball.GetRadius(), ball.GetColor()); // Use the updated color name
+            }
+
+            DrawText("Congratulations! You win this match", screenWidth / 7, screenHeight / 2, 45, BLACK); // text x y font size
+
+            EndDrawing();
+        }
+    }
+};
+
 
 int main()
 {   
@@ -266,15 +333,23 @@ int main()
 
     player2 c(10,sheight/2-50,25,130);
 
+    AnimateBall aniball;
+
 
     
     //game loop start 
 
-
-
-
     
-while (WindowShouldClose() ==false){
+    while (WindowShouldClose() ==false){
+
+    //game end in 5 rounds 
+     if (b.rounds()==3 ){
+        
+        break;
+     }
+    else
+        {
+
     BeginDrawing();
 
     //updating
@@ -307,6 +382,9 @@ while (WindowShouldClose() ==false){
     //Drawing 
     ClearBackground(DarkBlue);
 
+
+    DrawLine(0,0,swidth,0,WHITE);
+    DrawLine(0,sheight,swidth,sheight,WHITE);
     DrawRectangle(swidth/2,0,swidth/2,sheight,Blue);
     DrawCircle( swidth/2 , sheight /2 ,150 , LightB );
     DrawLine(swidth/2,0,swidth/2,sheight, WHITE);
@@ -314,25 +392,34 @@ while (WindowShouldClose() ==false){
    // DrawRectangle(10,sheight/2-50,25,100,GREEN);
     c.Draw();
     p.Draw();
-    DrawLine(0,0,swidth,0,WHITE);
-    DrawLine(0,sheight,swidth,sheight,WHITE);
-
+    
       DrawText(TextFormat("player 1 :%i", player2_score), swidth / 4 - 95, 20, 50, WHITE);    // text x y font size color
       DrawText(TextFormat("player 2: %i", player1_score),835 , 20, 50, WHITE);                  // text x y font size color
 
       DrawText( "ROUND ",swidth/2-25 , sheight /2-70, 20, roun);
 
-       DrawText(TextFormat("%i",b.rounds()) ,swidth/2-25 , sheight /2-50, 100, roun);  
+      DrawText(TextFormat("%i",b.rounds()) ,swidth/2-25 , sheight /2-50, 100, roun);     
 
     EndDrawing();
+   
+   }
 
+ }
+
+   while (WindowShouldClose() ==false){ 
+     BeginDrawing();
+       
     
+    aniball.animation();
+    break;
 
-    if (b.rounds()==5 ){
+      EndDrawing();
 
-   break;
-     }
-  }
+
+   }
+   
+
+ 
 
    //UnloadSound( strike);
     CloseAudioDevice();
